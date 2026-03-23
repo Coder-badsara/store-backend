8d748c9bc8e14f4470f96e73dc055570594c4c09
UMESH BADSARA <umesh2505badsara@gmail.com>
2026-03-23 15:14:18 +0530

    added some views

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product_list'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<int:id>/', views.products_detail, name='product_detail'),
 ]
