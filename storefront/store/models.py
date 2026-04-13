1f73b6b37d984f64e7140fef7d1e536869195e6f
UMESH BADSARA <umesh2505badsara@gmail.com>
2026-04-13 13:56:42 +0530

    Added jwt to the project

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
@@ -61,8 +62,11 @@ class Customer(models.Model):
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
     
+    @admin.display(ordering='user__first_name')
     def first_name(self):
         return self.user.first_name
+    
+    @admin.display(ordering='user__last_name')
     def last_name(self):
         return self.user.last_name
     class Meta:
@@ -82,6 +86,11 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+    class Meta:
+        permissions = [
+            ('cancel_order','Can cancel order')
+        ]
     
     def __str__(self):
         return f"Order {self.id}"
